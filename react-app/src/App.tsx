5d9680e48b29274204d65510ef536e0de13ef625
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2024-11-26 22:58:41 +0530

    lerning the passing children part in typesript eith react

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New york", "San Francisco", "tokyo", "London", "Paris"];
@@ -8,7 +9,12 @@ function App() {
     return (
     <div>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Alert>
+        Hello world
+      </Alert>
+     
     </div>
+
   );
 }
 
