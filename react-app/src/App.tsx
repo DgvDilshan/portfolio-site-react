156f9736d6b2127a75ec35831b281ecd15b888be
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2024-11-27 10:47:41 +0530

    lerning the alert massage in adiing button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   let items = ["New york", "San Francisco", "tokyo", "London", "Paris"];
     
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
     return (
+       
     <div>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-      <Alert>
-        Hello world
-      </Alert>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>
+        My alert
+      </Alert>}
       
-        <Button color="primary" onClick={() => console.log('clicked')}>
+        <Button color="primary" onClick={() =>  setAlertVisibility(true)}>
           Submit
         </Button>
      
